cc504455b43e72445a4e2abbebae7e7f12930699
Martin Larralde <martin.larralde@ens-cachan.fr>
2017-05-20 22:22:32 +0200

    Reorganize window header code

--- src/ui/header.c
+++ src/ui/header.c
@@ -56,11 +56,11 @@ static void torrent_page_on_refresh_clicked(GtkButton *button, gpointer callback
 static GtkWidget* torrent_page_new_header(TorrentPage* page) {
 
   GtkWidget* header_hbox    = gtk_hbox_new(FALSE, 0);
-  GtkWidget* header_icon    = gtk_image_new_from_icon_name("application-torrent", GTK_ICON_SIZE_DIALOG);
+  GtkWidget* info_vbox      = gtk_vbox_new(TRUE, 0);
+  GtkWidget* button_hbox    = gtk_hbox_new(TRUE, 0);
+  GtkWidget* stats_hbox     = gtk_hbox_new(FALSE, 10);
   GtkWidget* header_title   = gtk_label_new("Torrent name");
-  GtkWidget* stats_vbox     = gtk_vbox_new(TRUE, 0);
-  GtkWidget* seeders_hbox   = gtk_hbox_new(TRUE, 0);
-  GtkWidget* leechers_hbox  = gtk_hbox_new(TRUE, 0);
+  GtkWidget* header_image   = gtk_image_new_from_icon_name("application-torrent", GTK_ICON_SIZE_DIALOG);
   GtkWidget* seeders_label  = gtk_label_new("<b>Seeders:</b>");
   GtkWidget* leechers_label = gtk_label_new("<b>Leechers:</b>");
   GtkWidget* seeders_value  = gtk_label_new("<span foreground='green'><b>N/A</b></span>");
@@ -69,30 +69,31 @@ static GtkWidget* torrent_page_new_header(TorrentPage* page) {
 
   g_signal_connect(G_OBJECT(refresh_button), "clicked", torrent_page_on_refresh_clicked, page);
 
-  gtk_label_set_use_markup (GTK_LABEL(seeders_label), TRUE);
-  gtk_label_set_use_markup (GTK_LABEL(leechers_label), TRUE);
-  gtk_label_set_use_markup (GTK_LABEL(seeders_value), TRUE);
-  gtk_label_set_use_markup (GTK_LABEL(leechers_value), TRUE);
+  gtk_label_set_use_markup(GTK_LABEL(seeders_label),  TRUE);
+  gtk_label_set_use_markup(GTK_LABEL(leechers_label), TRUE);
+  gtk_label_set_use_markup(GTK_LABEL(seeders_value),  TRUE);
+  gtk_label_set_use_markup(GTK_LABEL(leechers_value), TRUE);
 
-  gtk_misc_set_alignment(GTK_MISC(seeders_label), 1.0f, 0.5f);
-  gtk_misc_set_alignment(GTK_MISC(leechers_label), 1.0f, 0.5f);
+  gtk_misc_set_alignment(GTK_MISC(header_title),   0.0f, 0.5f);
+  gtk_misc_set_alignment(GTK_MISC(seeders_label),  0.0f, 0.5f);
+  gtk_misc_set_alignment(GTK_MISC(seeders_value),  0.0f, 0.5f);
+  gtk_misc_set_alignment(GTK_MISC(leechers_label), 0.0f, 0.5f);
+  gtk_misc_set_alignment(GTK_MISC(leechers_value), 0.0f, 0.5f);
 
-  gtk_label_set_line_wrap (GTK_LABEL(header_title), TRUE);
+  gtk_box_pack_start(GTK_BOX(button_hbox), refresh_button, FALSE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(stats_hbox),  seeders_label,  FALSE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(stats_hbox),  seeders_value,  FALSE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(stats_hbox),  leechers_label, FALSE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(stats_hbox),  leechers_value, FALSE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(info_vbox),   header_title,   TRUE,  TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(info_vbox),   stats_hbox,     TRUE, FALSE, 0);
+  gtk_box_pack_start(GTK_BOX(header_hbox), header_image,   FALSE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(header_hbox), info_vbox,      TRUE,  TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(header_hbox), button_hbox,    FALSE, TRUE, 0);
 
-  gtk_box_pack_start(GTK_BOX(header_hbox), header_icon, FALSE, TRUE, 0);
-  gtk_box_pack_start(GTK_BOX(header_hbox), header_title, TRUE, TRUE, 5);
-  gtk_box_pack_start(GTK_BOX(header_hbox), stats_vbox, TRUE, TRUE, 0);
-  gtk_box_pack_start(GTK_BOX(header_hbox), refresh_button, FALSE, TRUE, 0);
-  gtk_box_pack_start(GTK_BOX(stats_vbox), seeders_hbox, TRUE, TRUE, 0);
-  gtk_box_pack_start(GTK_BOX(stats_vbox), leechers_hbox, TRUE, TRUE, 0);
-  gtk_box_pack_start(GTK_BOX(seeders_hbox), seeders_label, FALSE, TRUE, 0);
-  gtk_box_pack_start(GTK_BOX(seeders_hbox), seeders_value, FALSE, TRUE, 0);
-  gtk_box_pack_start(GTK_BOX(leechers_hbox), leechers_label, FALSE, TRUE, 0);
-  gtk_box_pack_start(GTK_BOX(leechers_hbox), leechers_value, FALSE, TRUE, 0);
-
-  page->refresh = refresh_button;
-  page->title = header_title;
-  page->seeders = seeders_value;
+  page->title    = header_title;
+  page->refresh  = refresh_button;
+  page->seeders  = seeders_value;
   page->leechers = leechers_value;
 
   return header_hbox;
