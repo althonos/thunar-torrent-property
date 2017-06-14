ea48097fa080d5f012f178811aac5bdbe7449c48
Martin Larralde <martin.larralde@ens-cachan.fr>
2017-06-14 17:05:01 +0200

    Have files displayed as a Tree instead of list

--- src/ui/files.c
+++ src/ui/files.c
@@ -1,3 +1,5 @@
+#include <string.h>
+#include <stdlib.h>
 #include <gio/gio.h>
 
 
@@ -26,24 +28,119 @@ static GtkWidget* torrent_page_new_files_view(TorrentPage* page) {
 }
 
 
+
+
+
+
+typedef struct _Entry Entry;
+struct _Entry{
+  Entry* child;
+  Entry* brother;
+  char* name;
+  size_t size;
+};
+
+static Entry* new_entry(char* name) {
+  Entry* entry = (Entry*) malloc(sizeof(Entry));
+  entry->child = NULL;
+  entry->brother = NULL;
+  entry->name = strdup(name);
+  entry->size = 0;
+  return entry;
+}
+
+static void add_entry(Entry* root, char* path, size_t size) {
+
+  Entry* entry = root;
+  char* path_copy = strdup(path);
+
+  char* partial_name = strtok(path_copy, "/");
+  while (partial_name != NULL) {
+    printf ("%s\n",partial_name);
+
+    if (entry->child == NULL) {
+      entry->child = new_entry(partial_name);
+      entry = entry->child;
+
+    } else if (strcmp(entry->child->name, partial_name) == 0){
+      entry = entry->child;
+
+    } else {
+
+      Entry* brother = entry->child;
+
+      while ((brother->brother != NULL) && (strcmp(brother->brother->name, partial_name) != 0)) {
+        brother = brother->brother;
+      }
+
+      if (brother->brother == NULL)
+        brother->brother = new_entry(partial_name);
+
+      entry = brother->brother;
+    }
+
+    char* aux = partial_name;
+    partial_name = strtok (NULL, "/");
+  }
+
+  free(path_copy);
+  entry->size = size;
+}
+
+static void free_entry(Entry* entry) {
+  free(entry->name);
+
+  if ((entry->child) != NULL)
+    free(entry->child);
+  if ((entry->brother) != NULL)
+    free(entry->brother);
+
+  free(entry);
+}
+
+
+static void fill_tree_store(Entry* entry, GtkTreeIter* parent, GtkTreeStore* store) {
+
+  printf("Adding %s to tree store\n", entry->name);
+
+  GtkTreeIter iter;
+  printf("Adding new row\n");
+  gtk_tree_store_append(store, &iter, parent);
+
+  printf("Setting values: %s (%i B)\n", entry->name, entry->size);
+  gtk_tree_store_set(GTK_TREE_STORE(store), &iter, 0, entry->name, 1, entry->size, -1);
+
+  if (entry->brother != NULL)
+    fill_tree_store(entry->brother, parent, store);
+  if (entry->child != NULL)
+    fill_tree_store(entry->child, &iter, store);
+}
+
+
+
+
+
+
 static void torrent_page_set_files(TorrentPage* page, long int size, char** files, long int* sizes) {
 
   //GtkListStore* new_list_store = gtk_list_store_new(3, GTK_TYPE_STRING, GTK_TYPE_STRING, GTK_TYPE_STRING);
-  GtkListStore* new_list_store = gtk_list_store_new(2, GTK_TYPE_STRING, GTK_TYPE_LONG);
+  GtkTreeStore* new_store = gtk_tree_store_new(2, GTK_TYPE_STRING, GTK_TYPE_LONG);
+
   gtk_tree_view_set_model(GTK_TREE_VIEW(page->files), NULL);
 
-  GtkTreeIter iter;
+  Entry* root = new_entry("/");
   for (int i=0; i < size; i++) {
     #ifndef NDEBUG
       g_message(files[i]);
     #endif
-    gtk_list_store_append(GTK_LIST_STORE(new_list_store), &iter);
-    gtk_list_store_set(GTK_LIST_STORE(new_list_store), &iter, 0, files[i], 1, sizes[i], -1);
+    add_entry(root, files[i], sizes[i]);
   }
 
-  gtk_tree_view_set_model(GTK_TREE_VIEW(page->files), GTK_TREE_MODEL(new_list_store));
-  g_object_unref(G_OBJECT(new_list_store));
+  fill_tree_store(root->child, NULL, new_store);
 
+  gtk_tree_view_set_model(GTK_TREE_VIEW(page->files), GTK_TREE_MODEL(new_store));
+  g_object_unref(G_OBJECT(new_store));
+  free(root);
 }
 
 
