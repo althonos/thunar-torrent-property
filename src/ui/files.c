bbee333532d939c89abdd44c481f19ed035b3d1d
Martin Larralde <martin.larralde@ens-cachan.fr>
2017-06-19 16:12:25 +0200

    Show type icons in the file section (guess based on extension)

--- src/ui/files.c
+++ src/ui/files.c
@@ -1,6 +1,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <gio/gio.h>
+#include <gdk/gdk.h>
 
 #include "../utils/fs.h"
 
@@ -10,17 +11,17 @@ static GtkWidget* torrent_page_new_files_view(TorrentPage* page) {
   GtkCellRenderer* renderer = gtk_cell_renderer_text_new();
   GtkCellRenderer* icon_renderer = gtk_cell_renderer_pixbuf_new();
   GtkWidget* window = gtk_scrolled_window_new(NULL, NULL);
-  //GtkListStore* store = gtk_list_store_new(3, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_STRING);
-  GtkListStore* store = gtk_list_store_new(2, G_TYPE_STRING, GTK_TYPE_LONG);
+  GtkTreeStore* store = gtk_list_store_new(3, G_TYPE_ICON, G_TYPE_STRING, G_TYPE_LONG);
+  //GtkListStore* store = gtk_list_store_new(2, G_TYPE_STRING, GTK_TYPE_LONG);
   GtkWidget* tree_view = gtk_tree_view_new_with_model(GTK_TREE_MODEL(store));
   g_object_unref(G_OBJECT(store));
 
   GtkTreeViewColumn* column;
-  // column = gtk_tree_view_column_new_with_attributes("Icon", icon_renderer, "icon_name", 0, NULL);
-  // gtk_tree_view_append_column(GTK_TREE_VIEW(tree_view), column);
-  column = gtk_tree_view_column_new_with_attributes("Path", renderer, "text", 0, NULL);
+  column = gtk_tree_view_column_new_with_attributes("Type", icon_renderer, "gicon", 0, NULL);
   gtk_tree_view_append_column(GTK_TREE_VIEW(tree_view), column);
-  column = gtk_tree_view_column_new_with_attributes("Size (B)", renderer, "text", 1, NULL);
+  column = gtk_tree_view_column_new_with_attributes("Name", renderer, "text", 1, NULL);
+  gtk_tree_view_append_column(GTK_TREE_VIEW(tree_view), column);
+  column = gtk_tree_view_column_new_with_attributes("Size (B)", renderer, "text", 2, NULL);
   gtk_tree_view_append_column(GTK_TREE_VIEW(tree_view), column);
 
   page->files = tree_view;
@@ -39,8 +40,19 @@ static void fill_tree_store(Entry* entry, GtkTreeIter* parent, GtkTreeStore* sto
   printf("Adding new row\n");
   gtk_tree_store_append(store, &iter, parent);
 
+
+  GIcon* entry_icon;
+  printf("Getting icons of %s\n", entry->name);
+  if (entry->child != NULL) {  // Some child --> this is a directory
+    entry_icon = g_icon_new_for_string("folder", NULL);
+  } else {
+    gchar* entry_type = g_content_type_guess (entry->name, NULL, 0, NULL);
+    entry_icon = g_content_type_get_icon(entry_type);
+    g_free(entry_type);
+  }
+
   printf("Setting values: %s (%i B)\n", entry->name, entry->size);
-  gtk_tree_store_set(GTK_TREE_STORE(store), &iter, 0, entry->name, 1, entry->size, -1);
+  gtk_tree_store_set(GTK_TREE_STORE(store), &iter, 0, entry_icon, 1, entry->name, 2, entry->size, -1);
 
   if (entry->brother != NULL)
     fill_tree_store(entry->brother, parent, store);
@@ -53,12 +65,13 @@ static void fill_tree_store(Entry* entry, GtkTreeIter* parent, GtkTreeStore* sto
 
 static void torrent_page_set_files(TorrentPage* page, Filesystem* fs) {
 
-  //GtkListStore* new_list_store = gtk_list_store_new(3, GTK_TYPE_STRING, GTK_TYPE_STRING, GTK_TYPE_STRING);
-  GtkTreeStore* new_store = gtk_tree_store_new(2, GTK_TYPE_STRING, GTK_TYPE_LONG);
+  GtkTreeStore* new_store = gtk_tree_store_new(3, G_TYPE_ICON, G_TYPE_STRING, G_TYPE_LONG);
+  //GtkTreeStore* new_store = gtk_tree_store_new(2, GTK_TYPE_STRING, GTK_TYPE_LONG);
 
   gtk_tree_view_set_model(GTK_TREE_VIEW(page->files), NULL);
 
   fill_tree_store(fs->root->child, NULL, new_store);
+
 
   gtk_tree_view_set_model(GTK_TREE_VIEW(page->files), GTK_TREE_MODEL(new_store));
   g_object_unref(G_OBJECT(new_store));
