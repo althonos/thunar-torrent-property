ac26ae6a033610b159a75816401454344f723492
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-12-30 02:24:56 +0100

    Fix deprecation warnings in `src/ui/header.c` and remove torrent name from header

--- src/ui/header.c
+++ src/ui/header.c
@@ -63,18 +63,17 @@ static void torrent_page_on_refresh_clicked(GtkButton *button, gpointer callback
 
 static GtkWidget* torrent_page_new_header(TorrentPage* page) {
 
-  GtkWidget* header_hbox    = gtk_hbox_new(FALSE, 0);
-  GtkWidget* info_vbox      = gtk_vbox_new(TRUE, 0);
-  GtkWidget* button_hbox    = gtk_hbox_new(TRUE, 0);
-  GtkWidget* stats_hbox     = gtk_hbox_new(FALSE, 10);
+  GtkWidget* header_hbox    = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
+  GtkWidget* stats_grid     = gtk_grid_new();
   GtkWidget* header_title   = gtk_label_new("Torrent name");
   GtkWidget* header_image   = gtk_image_new_from_icon_name("application-torrent", GTK_ICON_SIZE_DIALOG);
-  GtkWidget* seeders_label  = gtk_label_new("<b>Seeders:</b>");
-  GtkWidget* leechers_label = gtk_label_new("<b>Leechers:</b>");
+  GtkWidget* seeders_label  = gtk_label_new("<b>Seeders: </b>");
+  GtkWidget* leechers_label = gtk_label_new("<b>Leechers: </b>");
   GtkWidget* seeders_value  = gtk_label_new("<span foreground='green'><b>N/A</b></span>");
   GtkWidget* leechers_value = gtk_label_new("<span foreground='red'><b>N/A</b></span>");
-  GtkWidget* refresh_button = gtk_button_new_from_stock(GTK_STOCK_REFRESH);
+  GtkWidget* refresh_button = gtk_button_new_from_icon_name("view-refresh", GTK_ICON_SIZE_BUTTON);
 
+  gtk_button_set_label (GTK_BUTTON(refresh_button), "Refresh");
   g_signal_connect(G_OBJECT(refresh_button), "clicked", G_CALLBACK(torrent_page_on_refresh_clicked), page);
 
   gtk_label_set_use_markup(GTK_LABEL(seeders_label),  TRUE);
@@ -82,22 +81,17 @@ static GtkWidget* torrent_page_new_header(TorrentPage* page) {
   gtk_label_set_use_markup(GTK_LABEL(seeders_value),  TRUE);
   gtk_label_set_use_markup(GTK_LABEL(leechers_value), TRUE);
 
-  gtk_misc_set_alignment(GTK_MISC(header_title),   0.0f, 0.5f);
-  gtk_misc_set_alignment(GTK_MISC(seeders_label),  0.0f, 0.5f);
-  gtk_misc_set_alignment(GTK_MISC(seeders_value),  0.0f, 0.5f);
-  gtk_misc_set_alignment(GTK_MISC(leechers_label), 0.0f, 0.5f);
-  gtk_misc_set_alignment(GTK_MISC(leechers_value), 0.0f, 0.5f);
+  gtk_widget_set_halign(stats_grid, 0.0f);
+  gtk_widget_set_valign(stats_grid, 0.5f);
 
-  gtk_box_pack_start(GTK_BOX(button_hbox), refresh_button, FALSE, TRUE, 0);
-  gtk_box_pack_start(GTK_BOX(stats_hbox),  seeders_label,  FALSE, TRUE, 0);
-  gtk_box_pack_start(GTK_BOX(stats_hbox),  seeders_value,  FALSE, TRUE, 0);
-  gtk_box_pack_start(GTK_BOX(stats_hbox),  leechers_label, FALSE, TRUE, 0);
-  gtk_box_pack_start(GTK_BOX(stats_hbox),  leechers_value, FALSE, TRUE, 0);
-  gtk_box_pack_start(GTK_BOX(info_vbox),   header_title,   TRUE,  TRUE, 0);
-  gtk_box_pack_start(GTK_BOX(info_vbox),   stats_hbox,     TRUE, FALSE, 0);
-  gtk_box_pack_start(GTK_BOX(header_hbox), header_image,   FALSE, TRUE, 0);
-  gtk_box_pack_start(GTK_BOX(header_hbox), info_vbox,      TRUE,  TRUE, 0);
-  gtk_box_pack_start(GTK_BOX(header_hbox), button_hbox,    FALSE, TRUE, 0);
+  gtk_grid_attach(GTK_GRID(stats_grid), header_image, 0, 0, 1, 2);
+  gtk_grid_attach_next_to(GTK_GRID(stats_grid), seeders_label, header_image, GTK_POS_RIGHT, 1, 1);
+  gtk_grid_attach_next_to(GTK_GRID(stats_grid), seeders_value, seeders_label, GTK_POS_RIGHT, 1, 1);
+  gtk_grid_attach_next_to(GTK_GRID(stats_grid), leechers_label, seeders_label, GTK_POS_BOTTOM, 1, 1);
+  gtk_grid_attach_next_to(GTK_GRID(stats_grid), leechers_value, leechers_label, GTK_POS_RIGHT, 1, 1);
+
+  gtk_box_pack_start(GTK_BOX(header_hbox), stats_grid,      TRUE,  TRUE, 0);
+  gtk_box_pack_end(GTK_BOX(header_hbox), refresh_button,    FALSE, TRUE, 0);
 
   page->title    = header_title;
   page->refresh  = refresh_button;
@@ -105,4 +99,5 @@ static GtkWidget* torrent_page_new_header(TorrentPage* page) {
   page->leechers = leechers_value;
 
   return header_hbox;
+  // return header_grid;
 }
